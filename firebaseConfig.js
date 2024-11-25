af39e1e7ec90515d79f65bf41113e0c693cb1963
hghlnd <61725271+hghlnd@users.noreply.github.com>
2024-11-24 22:22:31 -0700

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFvIDR7rdC45TBAYVJQZHVSd55yXwlT88",
+  authDomain: "indedfirebaseassignment.firebaseapp.com",
+  projectId: "indedfirebaseassignment",
+  storageBucket: "indedfirebaseassignment.appspot.com",
+  messagingSenderId: "297837698432",
+  appId: "1:297837698432:web:49e7ede0aa4c474ba87565",
+  measurementId: "G-9CG1PL1ZR7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
