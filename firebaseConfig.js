5dbfe0484272533e0dd948afcb0a4961f38b3281
hghlnd <61725271+hghlnd@users.noreply.github.com>
2024-12-08 23:20:12 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,15 +3,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// Firebase configuration (your actual details)
+// Firebase configuration 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFvIDR7rdC45TBAYVJQZHVSd55yXwlT88",
-  authDomain: "indedfirebaseassignment.firebaseapp.com",
-  projectId: "indedfirebaseassignment",
-  storageBucket: "indedfirebaseassignment.appspot.com",
-  messagingSenderId: "297837698432",
-  appId: "1:297837698432:web:49e7ede0aa4c474ba87565",
-  measurementId: "G-9CG1PL1ZR7",
+  apiKey: "AIzaSyAZS9WJFORTCbXcyn3T2xsv-h_FnEOWn3k",
+  authDomain: "fir-assignment-43197.firebaseapp.com",
+  projectId: "fir-assignment-43197",
+  storageBucket: "fir-assignment-43197.appspot.com",
+  messagingSenderId: "477096400707",
+  appId: "1:477096400707:web:9f8e54a24df03f900041f6",
+  measurementId: "G-VPBW8C6Y8L",
 };
 
 // Initialize Firebase
@@ -25,4 +25,3 @@ if (app) {
 } else {
   console.error("Firebase initialization failed.");
 }
-
