a7228b8ea74c8f2c40e7fa699c3b8dc037fedd3c
hghlnd <61725271+hghlnd@users.noreply.github.com>
2024-12-11 17:38:32 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,17 @@
 // firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 // Firebase configuration 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZS9WJFORTCbXcyn3T2xsv-h_FnEOWn3k",
-  authDomain: "fir-assignment-43197.firebaseapp.com",
-  projectId: "fir-assignment-43197",
-  storageBucket: "fir-assignment-43197.appspot.com",
-  messagingSenderId: "477096400707",
-  appId: "1:477096400707:web:9f8e54a24df03f900041f6",
-  measurementId: "G-VPBW8C6Y8L",
+  apiKey: "AIzaSyD6bh0oFDuuhhT6CufKffE8t987qSKbRVw",
+  authDomain: "checkyourpocketlist-3638a.firebaseapp.com",
+  projectId: "checkyourpocketlist-3638a",
+  storageBucket: "checkyourpocketlist-3638a.firebasestorage.app",
+  messagingSenderId: "988679035896",
+  appId: "1:988679035896:web:326ba0404869ad850d540d",
+  measurementId: "G-YK8CK275V0"
 };
 
 // Initialize Firebase
